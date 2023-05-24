e15b32ff5c9ff47ddc0f925b486e940f56736644
agatha <agathakrivokon@gmail.com>
2023-05-24 17:08:22 +0300

    first commit

---
+++ src/main/java/com/example/tourgowebappfinalversion/TourGoWebAppFinalVersionApplication.java
@@ -0,0 +1,13 @@
+package com.example.tourgowebappfinalversion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TourGoWebAppFinalVersionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TourGoWebAppFinalVersionApplication.class, args);
+	}
+
+}
